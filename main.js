c389ddd6dc38015d1fb5f96273889c67f58d9375
kripersa <100306879+kripersa@users.noreply.github.com>
2023-09-14 13:22:08 +0400

    added project.html

---
+++ main.js
@@ -0,0 +1,13 @@
+// let projects = document.querySelector(".projects");
+// let rightSide = document.querySelector(".rightSide");
+// console.log(projects);
+// projects.addEventListener("click", function (e) {
+//   console.log("daeklika");
+//   rightSide.innerHTML = "";
+//   let projectContainer = document.createElement("div");
+//   projectContainer.classList.add("projectContainer");
+//   rightSide.appendChild(projectContainer);
+//   let title = document.createElement("h1");
+//   title.innerText = "My Projects";
+//   projectContainer.appendChild(title);
+// });
